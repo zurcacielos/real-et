9eb5b4cb9dcec2e7877e12c27bed151c4b621b80
zurcacielos <fabianse@gmail.com>
2026-09-09 09:53:34 -0400

    chore: rename fetch buttons

--- keyvaultcomparer-ui/src/App.vue
+++ keyvaultcomparer-ui/src/App.vue
@@ -624,8 +624,7 @@ const fetchValuesForVault = async (uri: string) => {
 
 const fetchButtonText = computed(() => {
   if (loading.value) return 'Fetching...';
-  const limit = uiSettings.value.resultLimit;
-  return limit > 0 ? `Fetch ${limit} Values` : 'Fetch All Values';
+  return "Fetch Visible Row's Values";
 });
 
 const fetchButtonTitle = computed(() => {
@@ -954,7 +953,7 @@ const getCellClasses = (statusObj: SecretValueStatus | undefined) => {
               <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor">
                 <path fill-rule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clip-rule="evenodd" />
               </svg>
-              Refetch Names
+              Refetch Secret Names
             </button>
             <button 
               v-if="vaultUris.length > 0"
